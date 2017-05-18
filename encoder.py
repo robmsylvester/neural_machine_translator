d35d76c5108e69dd2ebf281aad023bd87c25db7c
Rob Sylvester <robmsylvester@gmail.com>
2017-05-17 17:17:40 -0800

    still writing encoder loop...unstable now

--- encoder.py
+++ encoder.py
@@ -10,6 +10,19 @@ from tensorflow.python.ops import variable_scope
 
 FLAGS = tf.app.flags.FLAGS
 
+def _create_encoder_cell(json_layer_parameters):
+  if json_layer_parameters['lstm']:
+    return _create_encoder_lstm(json_layer_parameters['hidden_size'],
+                                json_layer_parameters['peepholes'],
+                                json_layer_parameters['init_forget_bias'],
+                                json_layer_parameters['dropout_keep_prob'])
+  else:
+    return _create_encoder_gru(json_layer_parameters['hidden_size'],
+                                json_layer_parameters['peepholes'],
+                                json_layer_parameters['init_forget_bias'],
+                                json_layer_parameters['dropout_keep_prob'])
+
+
 def _create_encoder_lstm(hidden_size, use_peepholes, init_forget_bias, dropout_keep_prob):
   c = core_rnn_cell_impl.LSTMCell(hidden_size, #number of units in the LSTM
                 use_peepholes=use_peepholes,
@@ -32,26 +45,87 @@ def run_encoder_NEW(encoder_json,
 
 
   # embeddings need to become non-updated-by-backprop embeddings from unsupervised glove, word2vec, or fasttext options
-  embeddings = tf.get_variable("encoder_embeddings",
-                              shape=[num_encoder_symbols, embedding_size],
-                              initializer=tf.random_uniform_initializer(-1.0, 1.0),
-                              dtype=dtype)
+  with variable_scope.variable_scope("encoder", dtype=dtype) as scope:
+    dtype=scope.dtype
 
-  #get the embedded inputs from the lookup table
-  embedded_encoder_inputs = tf.nn.embedding_lookup(embeddings, encoder_inputs)
-  current_layer = 0
-  cells = {}
-  outputs = {}
+    #create embeddings - this will eventually be moved elsewhere when the embeddings are no longer trained
+    with variable_scope.variable_scope("embeddings") as scope:
+      embeddings = tf.get_variable("encoder_embeddings",
+                                  shape=[num_encoder_symbols, embedding_size],
+                                  initializer=tf.random_uniform_initializer(-1.0, 1.0),
+                                  dtype=dtype)
 
-  #TODO - this NEEDS to become dynamic rnn
-  for layer_name, layer_parameters in encoder_json["layers"].iteritems():
-    inputs = inputs or embedded_encoder_inputs
+      #get the embedded inputs from the lookup table
+      embedded_encoder_inputs = tf.nn.embedding_lookup(embeddings, encoder_inputs)
+
+    current_layer = 0
+    cell_outputs_states = {} #indexed by layer name in json, which stores a dict with keys "output" and "state"
+
+    #TODO - this NEEDS to become dynamic rnn
+    for layer_name, layer_parameters in encoder_json["layers"].iteritems():
+
+      #cell outputs and states are created. if unidirectional gru or lstm, out is still a list.
+      #if bidirectional, it is a list of two tensors, unless they are concatenated
+      cell_outputs[layer_name] = []
+
+      #TODO, take this and the loop and refactor into a get_inputs function
+      #first loop will get embeddings, later loops will use previous iteration outputs, plus any residual connections
+      input_list = []
+
+      #add to inputs the residual connections that connect to this layer
+      for candidate_layer_name, candidate_layer_output in cell_outputs.iteritems():
+        if candidate_layer_name == layer_name: #layers don't have residual connections to themselves...
+          continue
+        elif candidate_layer_name in layer_parameters['input_layers'] #we will gather the layers that are marked in the architecture
+          print("Layer %s will use input from layer %s" % (layer_name, candidate_layer_name))
+          input_list.append(candidate_layer_output)
+
+      if len(input_list) == 0:
+        print("layer %s has no inputs in the input list. it will use the embeddings" % layer_name)
+        inputs = embedded_encoder_inputs
+
+      #combine these residual inputs following this layer's merge mode
+      if layer_parameters['input_merge_mode'] == 'concat':
+      elif layer_parameters['input_merge_mode'] == 'sum':
+      else:
+        raise ValueError("Only concat and sum are built for input merge modes in the runtime encoder stack loop, so far...")
+
+      #create/get the cells, and run them
+      #this will give us the outputs, and we can combine them as necessary
+      #c stands for cell, out for outputs, f for forward, b for backward
+      if layer_parameters['bidirectional']:
+        cf = _create_encoder_cell(layer_parameters)
+        cb = _create_encoder_cell(layer_parameters)
+
+        out_fb, out_f, out_b, state_f, state_b = core_rnn.static_bidirectional_rnn(cf, cb, inputs, dtype=dtype)
+        print("shape of out_forward for layer %s is %s " % (layer_name, str(out_f.get_shape())))
+
+        #store the outputs according to how they have to be merged.
+        #they will be a list with 2 elements, the forward and backward outputs. or, a list with one element, the concatenation or sum of the 2 elements.
+        if layer_parameters['output_merge_mode'] == 'concat':
+          assert out_f.get_shape().ndims == 2
+          cell_outputs_states[layer_name].append(tf.concat([out_f, out_b], axis=1)) #TODO - need to unstack here?
+        elif layer_parameters['output_merge_mode'] == 'sum':
+          cell_outputs_states[layer_name].append(tf.unstack(tf.add_n([out_f, out_b]))) #TODO - need to unstack here?
+        else:
+          cell_outputs_states[layer_name].append(out_f)
+          cell_outputs_states[layer_name].append(out_b)
+
+      else:
+        cf = _create_encoder_cell(layer_parameters)
+        out_f, state_f = core_rnn.static_rnn(cf, inputs, dtype=dtype)
+        cell_outputs_states[layer_name]["out"].append(out_f)
+
+      #we do care about the states, but only the top most state, which is what is stored in state_f
+
+
+
 
     #with variable_scope(layer_name, dtype=dtype) as scope:
     #   create_encoder_cell(layer_parameters)
 
     #current_layer += 1
-  pass
+    pass
 
 
 #TODO - modularize this from javascript object and replace with the functions above
